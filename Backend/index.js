1151d3eb1b3a653f74a70083d30bf46b5ae56e0f
Abha Meshram <abha25meshram@gmail.com>
2025-01-11 13:32:42 +0530

    added backend

--- Backend/index.js
+++ Backend/index.js
@@ -14,6 +14,22 @@ const corsOptions = {
   };
 app.use(cors(corsOptions))
 
+app.get('/test-network', async (req, res) => {
+    const url = `https://api.github.com/users/octocat`;
+    try {
+        const response = await axios.get(url, { timeout: 10000 }); // 10 seconds timeout
+        res.status(200).json({
+            message: "Network connectivity is fine",
+            data: response.data,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to connect to the GitHub API",
+            error: error.response?.data || error.message,
+        });
+    }
+});
+
 app.get("/",(req,res)=>{
     res.send("Hello from Autonomize Backend!!!");
 })
